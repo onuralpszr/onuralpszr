39d87158076320d1225f7c72fbbf4f0cf5d4ebac
Onuralp SEZER <thunderbirdtr@gmail.com>
2026-02-23 10:17:17 +0300

    feat: update site URL and enhance page title for better branding

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://onuralpszr.github.io',
+  site: 'https://onuralpsezer.com',
   base: '/',
   integrations: [sitemap()],
 });
